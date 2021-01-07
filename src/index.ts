341a39fe3322ec4268c3972028b0623d1501e9ef
bwapes <benwaples@gmail.com>
2021-01-06 17:39:53 -0800

    clean up index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,6 @@ const port = 8080 || process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api/v1/vegetables', vegetables)
 
-app.get('/', (req: Request, res: Response) => {
-  console.log('you did it')
-  res.send("<h1>Hello World</h1>")
-})
-
 app.listen(port, () => {
   console.log(`server is listening on port: ${port}`)
 })

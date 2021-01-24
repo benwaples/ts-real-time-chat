2eb4d262fa4f71ed2d5d2566d23f77b9e2d8c5df
bwapes <benwaples@gmail.com>
2021-01-24 14:12:46 -0800

    usda api for vegetagbles

--- src/endpoints/vegetables.ts
+++ src/endpoints/vegetables.ts
@@ -1,9 +1,24 @@
 import { Router, Request, Response } from 'express';
+import { Fruit, JsonData } from "../types";
+import { Response as FetchResponse} from 'node-fetch';
+
+const fetch = require('node-fetch')
+
 
 const vegetables = Router();
 
+const FDC_KEY = 'O3IfQiIhK9maV6b09F0zTcuUVLRFy5bIdSBoTuQd';
+
 vegetables.get('/', (req: Request, res: Response) => {
   res.send('<h1>Vegetables</h1>')
+})
+
+vegetables.get('/:name', async(req: Request, res: Response) => {
+
+  const json = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${FDC_KEY}&query=${req.params.name as string}`)
+  .then((data: FetchResponse): Promise<Fruit> => (data.json() as Promise<Fruit>))
+
+  res.send(json)
 })
 
 export { vegetables }

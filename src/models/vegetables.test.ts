7104fd2fc51d0adc1dce390940049252dfce469f
bwapes <benwaples@gmail.com>
2021-01-30 17:27:22 -0800

    refactor vegetable to insert veggies before tests run

--- src/models/vegetables.test.ts
+++ src/models/vegetables.test.ts
@@ -1,19 +1,25 @@
 export {}
 
+import { vegetable1, vegetable2 } from "../helper"
 import { Vegetables } from "./vegetables"
 const fs = require('fs')
 const pool = require('../utils/pool.ts')
 
 describe('Vegetables class', () => {
-  beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+  beforeEach(async () => {
+
+    await pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+
+    await Vegetables.insert(vegetable1)
+    
+    return await Vegetables.insert(vegetable2)
   })
 
   it('should insert a vegetable into the vegetable', async () => {
     const vegetable: Vegetables = {
       id: 3,
       genus: 'topic',
-      name: 'ben',
+      name: 'Zuccinni',
       family: 'team',
       order: 'quick',
       nutritions: {
@@ -27,44 +33,13 @@ describe('Vegetables class', () => {
 
     const actual = await Vegetables.insert(vegetable)
 
-    const result = await pool.query('SELECT * FROM vegetables')
+    const { rows } = await pool.query('SELECT * FROM vegetables where id=$1', [vegetable.id])
 
-    return expect(actual).toEqual(actual)
+    return expect(actual).toEqual(rows[0])
   });
 
   it('should return an array of all vegetables', async() => {
-    const vegetable1: Vegetables = {
-      id: 3,
-      genus: 'topic',
-      name: 'ben',
-      family: 'team',
-      order: 'quick',
-      nutritions: {
-        carbohydrates: 4,
-        protein: 3, 
-        fat: 6, 
-        calories: .3,
-        sugar: .5
-      }
-    }
-
-    await Vegetables.insert(vegetable1)
-    const vegetable2: Vegetables = {
-      id: 3,
-      genus: 'topic',
-      name: 'ben2',
-      family: 'team',
-      order: 'quick',
-      nutritions: {
-        carbohydrates: 4,
-        protein: 3, 
-        fat: 6, 
-        calories: .3,
-        sugar: .5
-      }
-    }
-    await Vegetables.insert(vegetable2)
-
+    
     const actual = await Vegetables.findAll();
 
     console.log(actual)
